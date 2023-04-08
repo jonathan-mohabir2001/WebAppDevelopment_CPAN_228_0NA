c7d9e8547f90a4d06b0edcb8ccb02a44cbb545ee
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-04-08 09:32:58 -0400

    LAB 05 PUSHES - COMPLETED LAB 5 with : Pagination and sorting, and tweaking my FIGHTERSLIST Thymeleaf with a search bar to test the power of pagination

--- tekkenreborn/src/main/java/com/cpan252/tekkenreborn/controller/FighterListController.java
+++ tekkenreborn/src/main/java/com/cpan252/tekkenreborn/controller/FighterListController.java
@@ -10,7 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/fighters")
@@ -20,10 +23,26 @@ public class FighterListController {
   private FighterRepository fighterRepository;
 
   @GetMapping
-  public String listFighters(Model model) {
-    List<Fighter> fighters = fighterRepository.findAll();
-    model.addAttribute("fighters", fighters);
-    return "fightersList"; // <-- use the correct view name here
+  public String listFighters(
+      Model model,
+      @RequestParam(name = "page", defaultValue = "0") int page,
+      @RequestParam(name = "size", defaultValue = "10") int size,
+      @RequestParam(name = "sort", defaultValue = "name") String sortBy,
+      @RequestParam(name = "direction", defaultValue = "ASC") String direction,
+      // Add a 'search' request parameter for search queries
+      @RequestParam(name = "search", defaultValue = "") String search) {
+
+    PageRequest pageRequest = PageRequest.of(page, size, Sort.Direction.fromString(direction), sortBy);
+
+    // Use the searchByName method if a search query is provided, otherwise use
+    // findAll
+    Page<Fighter> fightersPage = search.isEmpty()
+        ? fighterRepository.findAll(pageRequest)
+        : fighterRepository.searchByName(search, pageRequest);
+
+    model.addAttribute("fightersPage", fightersPage);
+
+    return "fightersList";
   }
 
 }
--- tekkenreborn/src/main/java/com/cpan252/tekkenreborn/repository/FighterRepository.java
+++ tekkenreborn/src/main/java/com/cpan252/tekkenreborn/repository/FighterRepository.java
@@ -1,9 +1,16 @@
 package com.cpan252.tekkenreborn.repository;
 
 import com.cpan252.tekkenreborn.model.Fighter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FighterRepository extends JpaRepository<Fighter, Long> {
+
+  // Add a search method using a custom JPQL query
+  @Query("SELECT f FROM Fighter f WHERE f.name LIKE %:searchTerm%")
+  Page<Fighter> searchByName(String searchTerm, Pageable pageable);
 }
